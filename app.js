0c34f2cbdaa703605442547b19d5429cf5cce34c
d-d-wolfe <dave.wolfe623@gmail.com>
2020-04-01 10:50:15 -0700

    Cleaned up the code a bit and deleted some comments.

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 'use strict';
 
-//ask users name
-//return greeting "welcome to my page userName"
 var userName = prompt('What is your name?');
 //console.log(userName)
   alert('Welcome to my page ' + userName + '!')
 
-//have you gone to college
-//yes or no
 var wentToCollege = prompt('Has Dave gone to college ' + userName +'?' + ' Y or N').toUpperCase();
-//var wentToCollege2 = wentToCollege.toUpperCase
 if(wentToCollege === 'Y' ) {
 // console.log('Yes, ' + userName + ' , Dave did go to college.');
   alert('Yes, ' + userName + ' , Dave did go to college.');
@@ -18,8 +13,6 @@ if(wentToCollege === 'Y' ) {
   alert('Actually ' + userName + ' Dave did go to college.');
 }
 
-//have you worked in the software industry
-//yes or no
 var workedIndustry = prompt('Has Dave worked in the tech industry ' + userName +'?' + ' Y or N').toUpperCase();
 if(workedIndustry === 'Y') {
  // console.log('Yes, ' + userName + ' , Dave has worked in the industry.');
@@ -29,9 +22,6 @@ if(workedIndustry === 'Y') {
   alert('Actually, ' + userName + ' , Dave has worked in the tech industry.');
 }
 
-//Are you enjoying Code Fellows
-//yes or no
-//Im really enjoying it too userName
 var enjoyCodeFellows = prompt('Is Dave enjoying Code Fellows ' + userName +'?' + ' Y or N').toUpperCase();
 if(enjoyCodeFellows === 'Y') {
 //  console.log('Yes, ' + userName + ' , Dave is enjoying Code Fellows.');
@@ -41,8 +31,6 @@ if(enjoyCodeFellows === 'Y') {
   alert('Actually, ' + userName + ' , Dave is enjoying Code fellows.');
 }
 
-//Is your goal to become a JavaScript Developer like me?
-//yes or no
 var jsDeveloper = prompt('Is Daves goal to become a JavaScript Developer, ' + userName +'?' + ' Y or N').toUpperCase();
 if(jsDeveloper === 'Y') {
 //  console.log('Yes ' + userName + ' , Daves goal is to become a JavaScript Developer.');
